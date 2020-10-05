636228c7751dc1c8202c1216f3964e845dbd9f27
venkatesh <venkatesh.is.lekkala@gmail.com>
2020-10-05 23:55:29 +0200

    Service class Unit testing

--- src/test/java/com/tenera/weather/service/WeatherServiceTest.java
+++ src/test/java/com/tenera/weather/service/WeatherServiceTest.java
@@ -1,28 +1,73 @@
 package com.tenera.weather.service;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import com.tenera.weather.enums.WeatherTypesEnum;
+import com.tenera.weather.dto.WeatherDataDto;
+import com.tenera.weather.dto.integration.openWeather.Main;
+import com.tenera.weather.dto.integration.openWeather.OpenWeatherResult;
+import com.tenera.weather.dto.integration.openWeather.Weather;
+import com.tenera.weather.entity.WeatherData;
+import com.tenera.weather.repository.WeatherDataRepository;
+import com.tenera.weather.service.integration.openWeather.OpenweatherIntegrationService;
 
+@RunWith(MockitoJUnitRunner.class)
 public class WeatherServiceTest {
+	
+	private static OpenWeatherResult openWeatherResult;
 
-	static WeatherService weatherService;
+	private static WeatherService weatherService;
+
+	@Mock
+	private static WeatherDataRepository weatherDataRepository;
+
+	@Mock
+	private static OpenweatherIntegrationService openweatheIntegrationService;
 
 	@BeforeAll
 	static void initialize() {
-		
+		weatherService = new WeatherService(openweatheIntegrationService, weatherDataRepository);
+		openWeatherResult = initilaizeOpenWeatherResult();
 	}
 
 	@Test
-	public void testWeatherType() {
+	public void mapToWeatherDataDto_WeatherData() {
+		WeatherData weatherData = new WeatherData();
+		weatherData.setTemp(18.0d);
+		WeatherDataDto weatherDataDto = weatherService.mapToWeatherDataDto(weatherData);
+		assertEquals(18.0d,weatherDataDto.getTemp());
+	}
 
-		String rainWatherType = "Rain";
+	@Test
+	public void mapToWeatherDataDto_OpenWeatherResult() {
+		WeatherDataDto weatherDataDto = weatherService.mapToWeatherDataDto(openWeatherResult);
+		assertEquals(18.0d,weatherDataDto.getTemp());
+	}
 
-		assertTrue(WeatherTypesEnum.umbrellaWeatherType.contains(rainWatherType.toUpperCase()));
+	static OpenWeatherResult initilaizeOpenWeatherResult() {
+		OpenWeatherResult openWeatherResult = new OpenWeatherResult();
 
+		List<Weather> weatherList = new ArrayList<Weather>();
+		Weather weather = new Weather();
+		weather.setMain("Rain");
+		weatherList.add(weather);
+		
+		openWeatherResult.setWeather(weatherList);
+
+		Main main = new Main();
+		main.setTemp(18.0d);
+		main.setPressure(181);
+		openWeatherResult.setMain(main);
+
+		return openWeatherResult;
 	}
 
 }
